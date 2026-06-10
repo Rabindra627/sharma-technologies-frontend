1dc368c402dbd88ff2cffd4a14352e50a6ba4e59
RabindraSharma <sharmarabindra29@gmail.com>
2026-06-10 17:23:47 +0530

    connection updated

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = "mongodb+srv://sharmatech:Mongodb@26@cluster0.xwpbycu.mongodb.net/";
+const MONGODB_URI = "mongodb+srv://sharmatech:Mongodb@26@cluster0.xwpbycu.mongodb.net/sharmatechDb";
 
 export const connectDB = async () => {
   if (mongoose.connections[0].readyState) return;
 
   await mongoose.connect(MONGODB_URI);
 };
+
 
