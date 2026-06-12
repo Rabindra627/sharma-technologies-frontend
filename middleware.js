f0c571ad5a1e004d7dc603d82857f2e178e5dd9b
RabindraSharma <sharmarabindra29@gmail.com>
2026-06-12 16:56:46 +0530

    middleware token setting updated

--- middleware.js
+++ middleware.js
@@ -4,6 +4,13 @@ export function middleware(request) {
   const token = request.cookies.get("token")?.value;
 
   // Protect dashboard routes
+
+  // path
+    const path = request.nextUrl.pathname;
+    const isProtected = path.startsWith("/dashboard");
+
+    const isAuthPage = path === "/login";
+    
   if (
     !token &&
     request.nextUrl.pathname.startsWith("/dashboard")
@@ -11,6 +18,28 @@ export function middleware(request) {
     return NextResponse.redirect(
       new URL("/", request.url)
     );
+  }
+  if (token) {
+    try {
+      jwt.verify(
+        token,
+        process.env.JWT_SECRET
+      );
+
+      if (isAuthPage) {
+        return NextResponse.redirect(
+          new URL("/dashboard", request.url)
+        );
+      }
+    } catch {
+      const response = NextResponse.redirect(
+        new URL("/login", request.url)
+      );
+
+      response.cookies.delete("token");
+
+      return response;
+    }
   }
 
   return NextResponse.next();
