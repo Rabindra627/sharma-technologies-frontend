524a70f3c2978d88687889a3ac139bf8e8451e59
RabindraSharma <sharmarabindra29@gmail.com>
2026-06-12 14:04:35 +0530

    chnages

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -6,11 +6,9 @@ export default function Dashboard() {
   return (
 
     <div>
-
-      <h1 className="text-2xl font-bold mb-4">
+      <h1 className="text-2xl font-bold mb-2">
         Dashboard
       </h1>
-    
       <p className="text-gray-500 mb-2">
         Manage Users, Inquiries, Projects and Clients.
       </p> 
