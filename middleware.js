75ee0e52de88d1f4c91f70c48847e4681f70a224
RabindraSharma <sharmarabindra29@gmail.com>
2026-06-13 08:35:38 +0530

    middleware update

--- middleware.js
+++ middleware.js
@@ -2,55 +2,41 @@ import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 export function middleware(request) {
-  const token = request.cookies.get("token")?.value ||'';
+  const token = request.cookies.get("token")?.value;
 
-  // Protect dashboard routes
-  console.log("TOKEN : ",token);
-  // path
-    const path = request.nextUrl.pathname;
-    const isPublicPath = path === '/' || path === '/login' || path === 'signup';
+  const path = request.nextUrl.pathname;
 
-    
-    
-  if (isPublicPath && token) {
-    return NextResponse.redirect(
-      new URL("/dashboard", request.url)
-    );
-  }
-  if(!isPublicPath && !token){
-    return NextResponse.redirect(
-      new URL("/", request.url)
-    );
-  }
+  const isPublicPath =
+    path === "/" ||
+    path === "/login" ||
+    path === "/signup";
 
   if (token) {
     try {
-      jwt.verify(token,process.env.JWT_SECRET);
-
-      if (isPublicPath && token) {
-        return NextResponse.redirect(
-          new URL("/dashboard", request.url)
-        );
-      }
-    } catch(error) {
+      jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
       const response = NextResponse.redirect(
         new URL("/", request.url)
       );
+
       response.cookies.delete("token");
       return response;
     }
   }
 
+  // authenticated users shouldn't see auth pages
+  if (isPublicPath && token) {
+    return NextResponse.redirect(
+      new URL("/dashboard", request.url)
+    );
+  }
+
+  // unauthenticated users can't see protected pages
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(
+      new URL("/", request.url)
+    );
+  }
+
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-     "/dashboard",
-    "/dashboard/:path*", 
-    "/login",
-    "/signup",
-    "/profile"   
-  ],
-};
-
