55049eab3a4ea0cef2c6ce14486a356146e4785d
RabindraSharma <sharmarabindra29@gmail.com>
2026-07-09 19:07:16 +0530

    updt on 07.07

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/xirgmvgj/**', // Allows all paths under your Cloudinary account
+      },
+    ],
+  },
 };
 
 export default nextConfig;
